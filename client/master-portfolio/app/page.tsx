ca1f8f08edd8f86f7b8059fcaaa4b0f3264709d9
soikat-san <soikat.codes@gmail.com>
2025-07-15 23:22:02 +0530

    identity div

--- client/master-portfolio/app/page.tsx
+++ client/master-portfolio/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/src/blocks/hero";
+import Identity from "@/src/blocks/identity";
 import RadiantBackground from "@/src/blocks/radiance";
 
 export default function Home() {
   return (
     <RadiantBackground>
       <Hero />
+      <Identity />
     </RadiantBackground>
   );
 }
---
+++ client/master-portfolio/src/blocks/identity/index.tsx
@@ -0,0 +1,9 @@
+const Identity: React.FC = () => {
+  return (
+    <div className="mt-10 p-10 w-6/12 ml-auto rounded-4xl bg-white dark:bg-black">
+      hello
+    </div>
+  );
+};
+
+export default Identity;
